c2c2dfd142190768ff570b5fff4557fb2cf01599
Michael Alfandre <Michael Alfandre>
2019-04-12 18:39:29 -0600

    demo app complete. List view, scroll, like and save name, navigate to saved list.

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.cyan[600],
+      ),
       home: RandomWords(),
-      // Scaffold(
-      //   appBar: AppBar(
-      //     title: const Text('Welcome to Flutter'),
-      //   ),
-      //   body: new Center(
-      //     child: RandomWords(),
-      //   ),
-      // ),
     );
   }
 }
@@ -24,7 +19,39 @@ class MyApp extends StatelessWidget {
 class RandomWordsState extends State<RandomWords> {
   
     final List<WordPair> _suggestions = <WordPair>[];
+    final Set<WordPair> _saved = Set<WordPair>();
     final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+    void _pushSaved(){
+      Navigator.of(context).push(
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            final Iterable<ListTile> tiles = _saved.map(
+              (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  )
+                );
+              }
+            );
+            final List<Widget> divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+                .toList();
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Suggestoins'),
+              ),
+              body: ListView(children: divided)
+            );
+          }
+        )
+      );
+    }
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -59,11 +86,25 @@ class RandomWordsState extends State<RandomWords> {
         });
   }
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }
     );
   }
   @override
@@ -74,6 +115,9 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold (                   // Add from here... 
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );                                      // ... to here.
